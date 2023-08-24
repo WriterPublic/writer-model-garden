902486367ca869e1fe6819f5dc87052785180bb3
Mark Saad <3900312+mark-saad@users.noreply.github.com>
2023-08-24 20:29:07 +0000

    patch to lazy download the model

--- Dockerfile
+++ Dockerfile
@@ -21,12 +21,26 @@ WORKDIR /app
 COPY --from=base /app/requirements.txt /app/
 RUN python -m pip install --no-cache-dir -r requirements.txt
 
+# Install transformers from source.
+RUN git clone --depth 1 --branch v4.31.0 https://github.com/huggingface/transformers.git
+# The patch is used to change the transformers loading model behavior:
+# 1) For models on Huggingface hub: if the model has multiple shards, each shard
+#    will be downloaded separately and get deleted after loading to GPU.
+# 2) For models on local disk: if a model bin file is actually a text file
+#    recording a GCS path, the model file will be downloaded and get deleted
+#    after loading to GPU.
+COPY hf_transformers_lazy_download.patch .
+WORKDIR /app/transformers
+RUN git apply /app/hf_transformers_lazy_download.patch
+RUN pip install -e .
+
 # Set environment variables
 ARG HF_MODEL_ARG
 ENV HF_MODEL=$HF_MODEL_ARG
 ENV INFER_PORT=7080
 
 # Copy source files
+WORKDIR /app
 COPY server.py .
 COPY main.py .
 
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import os
 import logging
+import subprocess
 from typing import List, Dict
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 # Model input text
@@ -27,16 +28,23 @@ class HealthStatus(BaseModel):
 app = FastAPI()
 
 # Download model
-logging.info("Starting model download")
-hf_model = os.environ.get("HF_MODEL", "Writer/palmyra-base")
-model = pipeline(model = hf_model, task = "text-generation")
+logger = logging.getLogger("writer-model-garden")
+logger.setLevel(logging.INFO)
+logger.info("Printing Disk space stats")
+result = subprocess.run(['df', '-kh'], stdout=subprocess.PIPE)
+logger.info(result)
+
+hf_model = os.environ.get("HF_MODEL")
+logging.info(f"Starting model download {hf_model}")
+tokenizer = AutoTokenizer.from_pretrained(hf_model)
+model = AutoModelForCausalLM.from_pretrained(hf_model)
 logging.info("Model Downloaded")
 
 
 @app.post("/generate", response_model=ModelResponse)
 @app.post("/", response_model=ModelResponse)
 def generate(request: ModelRequest) -> ModelResponse:
-    prompts = [i["text"] for i in request.instances]
+    text = [i["text"] for i in request.instances]
     max_new_tokens = request.parameters.get("max_new_tokens", 200)
     temperature = request.parameters.get("temperature", 0.8)
     # temperature has to be a positive float
@@ -44,13 +52,12 @@ def generate(request: ModelRequest) -> ModelResponse:
     top_k = request.parameters.get("top_k", 50)
     top_p = request.parameters.get("top_p", 0.95)
     do_sample = request.parameters.get("do_sample", True)
-    outputs = model(prompts,
-                    max_new_tokens = max_new_tokens,
-                    temperature = temperature,
-                    top_k = top_k,
-                    top_p = top_p,
-                    do_sample = do_sample)
-    predictions = [out[0] for out in outputs]
+
+    inputs = tokenizer(text, return_tensors="pt").to(model.device)
+    outputs = model.generate(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'], max_new_tokens = max_new_tokens + 1, temperature = temperature, top_k = top_k, top_p = top_p, do_sample = do_sample)
+    encoded_results = [out[0] for out in outputs]
+    print(encoded_results)
+    predictions = [{"generated_text": tokenizer.decode(e, skip_special_tokens=True)} for e in encoded_results]
     return ModelResponse(predictions=predictions)
 
 
